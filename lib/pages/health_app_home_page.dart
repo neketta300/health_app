f3b2bec93891ee68529096939c0f328a5852517f
neketta300 <neketta300@gmail.com>
2025-02-07 20:27:10 +0300

    start home page screen

---
+++ lib/pages/health_app_home_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HealthAppHomePage extends StatelessWidget {
+  const HealthAppHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
